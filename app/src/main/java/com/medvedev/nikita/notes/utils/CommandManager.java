fc1124b40f6aafc7baba3d21b1803897dd02748a
Lirveez <lirveez@gmail.com>
2018-11-15 19:11:52 +0300

    New url-strings for requests

--- app/src/main/java/com/medvedev/nikita/notes/utils/CommandManager.java
+++ app/src/main/java/com/medvedev/nikita/notes/utils/CommandManager.java
@@ -1,8 +1,9 @@
 package com.medvedev.nikita.notes.utils;
 
 public class CommandManager {
-    public static final String REGISTER = "register";
-    public static final String LOGIN = "login";
-    public static final String ADD_NOTE = "add_note";
-    public static final String GET_NOTES = "get_notes";
+    private static final String server_url = "nikitamedvedev.ddns.net:8080/";
+    public static final String REGISTER = server_url+"register";
+    public static final String LOGIN = server_url+"login";
+    public static final String ADD_NOTE = server_url+"add_note";
+    public static final String GET_NOTES = server_url+"get_notes";
 }
