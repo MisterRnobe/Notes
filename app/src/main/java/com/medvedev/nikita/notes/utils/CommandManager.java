5f16222443dee0320096b822ecc34d62719901ff
Lirveez <lirveez@gmail.com>
2018-11-15 20:20:08 +0300

    CommandManager url bugfix

--- app/src/main/java/com/medvedev/nikita/notes/utils/CommandManager.java
+++ app/src/main/java/com/medvedev/nikita/notes/utils/CommandManager.java
@@ -1,7 +1,7 @@
 package com.medvedev.nikita.notes.utils;
 
 public class CommandManager {
-    private static final String server_url = "nikitamedvedev.ddns.net:8080/";
+    private static final String server_url = "http://nikitamedvedev.ddns.net:8080/";
     public static final String REGISTER = server_url+"register";
     public static final String LOGIN = server_url+"login";
     public static final String ADD_NOTE = server_url+"add_note";
