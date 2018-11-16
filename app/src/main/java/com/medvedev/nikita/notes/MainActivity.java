b2772617c4b16688fda23bc320cc4edaa80b2fdf
Lirveez <lirveez@gmail.com>
2018-11-16 04:10:34 +0300

    Logout functional

--- app/src/main/java/com/medvedev/nikita/notes/MainActivity.java
+++ app/src/main/java/com/medvedev/nikita/notes/MainActivity.java
@@ -1,29 +1,44 @@
 package com.medvedev.nikita.notes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
+
+import com.medvedev.nikita.notes.utils.SessionManager;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SessionManager session;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        session = new SessionManager(this);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> logoutUser());
+    }
+
+
+    @Override
+    protected void onStop() {
+        Log.i("STOPFROMSTOP","logout from stop");
+        super.onStop();
+        session.setLogin(false);
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    private void logoutUser() {
+        Log.i("LOGOUTFROMLOGOUT","logout from method");
+        session.setLogin(false);
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 }
