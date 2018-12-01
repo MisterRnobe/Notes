a1c3101492b1d048734662edf921601856c69a38
MisterRnobe <mr.rnobe@gmail.com>
2018-12-01 22:26:00 +0300

    Created new activity

--- app/src/main/java/com/medvedev/nikita/notes/NoteActivity.java
+++ app/src/main/java/com/medvedev/nikita/notes/NoteActivity.java
@@ -1,7 +1,11 @@
 package com.medvedev.nikita.notes;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class NoteActivity extends AppCompatActivity {
 
@@ -9,5 +13,8 @@ public class NoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+
 }
