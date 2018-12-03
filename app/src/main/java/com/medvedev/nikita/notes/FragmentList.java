45ebe69af22d94963cda5e235b11c47f580c2081
Lirveez <lirveez@gmail.com>
2018-12-03 04:54:28 +0300

    FragmentList final implementation

--- app/src/main/java/com/medvedev/nikita/notes/FragmentList.java
+++ app/src/main/java/com/medvedev/nikita/notes/FragmentList.java
@@ -1,17 +1,12 @@
 package com.medvedev.nikita.notes;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.medvedev.nikita.notes.objects.Note;
@@ -20,15 +15,11 @@ import com.medvedev.nikita.notes.objects.NotesRequest;
 import com.medvedev.nikita.notes.objects.Token;
 import com.medvedev.nikita.notes.utils.DBManager;
 import com.medvedev.nikita.notes.utils.ErrorManager;
+import com.medvedev.nikita.notes.utils.NoteAdapter;
 import com.medvedev.nikita.notes.utils.RequestManager;
 import com.medvedev.nikita.notes.utils.SharedPreferencesManager;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-
-import static android.content.Context.MODE_PRIVATE;
 
 public class FragmentList extends ListFragment {
 
@@ -51,9 +42,9 @@ public class FragmentList extends ListFragment {
     public void drawNotes(){
         DBManager myDB = new DBManager();
         List<Note> noteList = myDB.dbGetNotes().getNotes();
-        ArrayAdapter<Note> adapter = new ArrayAdapter<>(getActivity(),
-                android.R.layout.simple_list_item_1, noteList);
-        setListAdapter(adapter);
+        //ArrayAdapter<Note> adapter = new ArrayAdapter<>(getActivity(),
+             //   android.R.layout.simple_list_item_2, noteList);
+        setListAdapter(new NoteAdapter(getActivity(), noteList));
     }
     @Override
     public void onResume() {
@@ -67,7 +58,7 @@ public class FragmentList extends ListFragment {
         Note note = (Note)(getListAdapter()).getItem(position);
         Intent intent = new Intent(activity, NoteActivity.class);
         intent.putExtra("title",note.getTitle());
-        intent.putExtra("note",note.getNote());
+        intent.putExtra("noteText",note.getNote());
         intent.putExtra("note_id",note.getId());
         activity.startActivity(intent);
       }
